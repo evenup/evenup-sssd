3ab4f46e043ef30f195f4549512af07f33a2375e
Justin Lambert <jlambert@eml.cc>
2014-12-13 05:16:47 -0700

    normalize common files

--- manifests/config.pp
+++ manifests/config.pp
@@ -28,49 +28,49 @@ class sssd::config {
   }
 
   file { '/etc/pam.d/password-auth':
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0444',
-    source  => 'puppet:///modules/sssd/password-auth',
+    ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0444',
+    source => 'puppet:///modules/sssd/password-auth',
   }
 
   file { '/etc/pam.d/system-auth':
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0444',
-    source  => 'puppet:///modules/sssd/system-auth',
+    ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0444',
+    source => 'puppet:///modules/sssd/system-auth',
   }
 
   file { '/etc/nsswitch.conf':
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0444',
-    source  => 'puppet:///modules/sssd/nsswitch.conf',
+    ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0444',
+    source => 'puppet:///modules/sssd/nsswitch.conf',
   }
 
   case $sssd::logsagent {
     'beaver': {
       beaver::stanza { '/var/log/sssd/sssd_LDAP.log':
-        type    => 'sssd',
-        tags    => ['sssd', 'ldap', $::disposition],
+        type => 'sssd',
+        tags => ['sssd', 'ldap', $::disposition],
       }
 
       beaver::stanza { '/var/log/sssd/sssd.log':
-        type    => 'sssd',
-        tags    => ['sssd', $::disposition],
+        type => 'sssd',
+        tags => ['sssd', $::disposition],
       }
 
       beaver::stanza { '/var/log/sssd/sssd_nss.log':
-        type    => 'sssd',
-        tags    => ['sssd', 'nss', $::disposition],
+        type => 'sssd',
+        tags => ['sssd', 'nss', $::disposition],
       }
 
       beaver::stanza { '/var/log/sssd/sssd_pam.log':
-        type    => 'sssd',
-        tags    => ['sssd', 'pam', $::disposition],
+        type => 'sssd',
+        tags => ['sssd', 'pam', $::disposition],
       }
     }
     default: {}
